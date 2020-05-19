b28c9b19451f5d774b45b9d7be9be22d5951f293
Stathis <s.moraitidis@platomedia.com>
2020-05-19 13:14:20 +0100

    Add build/web

---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/assets/images/salmon.jpg": "f23261bdc0aa3436cb08b3557eeed4f6",
+"assets/AssetManifest.json": "a4231a0e9786f21a5a446dce6be3e38b",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/LICENSE": "34da31f697be5f2fcfacf877df9adb0a",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"main.dart.js": "972aeeb3a42621e2b77e68cf7eab51f3",
+"manifest.json": "b6c0465fa7828caf6461cd9bf57fd65a",
+"index.html": "e37c366db4d003d3f55da2d14565a541",
+"/": "e37c366db4d003d3f55da2d14565a541",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
